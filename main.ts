100be0b0ba9b4c9983a45951fc11cf0c28463f1a
niky19 <92981186+niky19@users.noreply.github.com>
2023-11-19 23:28:39 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
-if (input.lightLevel() > 200) {
-    music.play(music.stringPlayable("B A G A G F A C5 ", 120), music.PlaybackMode.UntilDone)
-} else if (input.lightLevel() < 50) {
-    music.play(music.stringPlayable("D C D E D C D D ", 120), music.PlaybackMode.UntilDone)
-}
+basic.forever(function () {
+    if (input.lightLevel() > 200) {
+        music.play(music.stringPlayable("B A G A G F A C5 ", 120), music.PlaybackMode.UntilDone)
+    } else if (input.lightLevel() < 50) {
+        music.play(music.stringPlayable("D C D E D C D D ", 120), music.PlaybackMode.UntilDone)
+    }
+})
